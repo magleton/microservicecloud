0205c43425b5e14e72fdd93b8d075871a1053e43
britton@126.com <macro123>
2019-10-28 21:48:11 +0800

    BUG FIXED

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/britton/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.britton.springcloud.service.impl;
+
+import com.britton.springcloud.dao.DeptDao;
+import com.atguigu.springcloud.entities.Dept;
+import com.britton.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by ZeroV on 2018/11/6.
+ */
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptDao dao;
+
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+
+}
---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/britton/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,39 @@
+package com.britton.springcloud.controller;
+
+import com.britton.springcloud.entities.Dept;
+import com.britton.springcloud.service.DeptClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by ZeroV on 2018/11/6.
+ */
+@RestController
+public class DeptController_Consumer {
+
+    @Autowired
+    DeptClientService service;
+
+    @RequestMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id)
+    {
+        return this.service.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list()
+    {
+        return this.service.list();
+    }
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public Object add(Dept dept)
+    {
+        return this.service.add(dept);
+    }
+
+}
